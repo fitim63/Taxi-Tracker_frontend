d7e7b718f79702d2b4a2638e70adcdb7472e96bb
Ereza Baftiu <eb40154@ubt-uni.net>
2020-04-08 22:50:59 +0200

    MapComponent.js

--- src/roots/Home.js
+++ src/roots/Home.js
@@ -1,8 +1,12 @@
 import React from "react";
+import MapComponent from "../components/MapComponent";
 // Functional component Home, do not have state.
 const Home = () => {
     return(
-        <div>Home.js</div>
+        <div>Home.js
+            <MapComponent />
+        </div>
+
     )
 };
 
