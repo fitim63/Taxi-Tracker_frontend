799ba2853f74255f982135b8f77cc1a49c58fb52
genthoxha <genthoxha123@gmail.com>
2020-04-12 14:24:26 +0200

    changes and fixes

--- src/roots/Login.js
+++ src/roots/Login.js
@@ -1,9 +1,7 @@
 import React from "react";
-import {Form, Button} from "react-bootstrap";
+import {Form, Button, Container} from "react-bootstrap";
 import '../components/style.css'
 
-// This is a Class Component that has state.
-// State is used to store data in component for example: (username, password) for making request to the api
 export default class Login extends React.Component {
 
     // state init with empty values
@@ -12,19 +10,14 @@ export default class Login extends React.Component {
         password: ''
     };
 
-    // get data from input and save it in react state
     handleChange(event) {
         this.setState({
             username: event.target.value,
         });
     }
-
     handleRegistration(event) {
 
     }
-
-    // this is render method where is written in JSX( almost same as html )
-    // everything that is here will be visible
 
     render() {
         return (
@@ -32,6 +25,9 @@ export default class Login extends React.Component {
                 <div className="col-md-3 m-5">
                     <Form>
                         <Form.Group controlId="formBasicPhoneNumber">
+                            <Container>
+                                <label className="text-center">Login</label>
+                            </Container>
                             <Form.Control
                                 className="m-2"
                                 type="text"
