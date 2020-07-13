2a03908889812455ed4e7beb6195b08f6a1e1160
genthoxha <genthoxha123@gmail.com>
2020-07-13 23:37:22 +0200

    merged erza's branch for popup in MapComponent.js

--- src/components/MapComponent/MapComponent.js
+++ src/components/MapComponent/MapComponent.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import "./Map.css";
 import L from "leaflet";
+import axios from "axios";
+import Button from "react-bootstrap/Button";
+import Loader from "react-loader-spinner";
 
 function MapComponent() {
   const [lat] = useState(52.505);
@@ -10,6 +13,7 @@ function MapComponent() {
   const [markersLat, setMarkerLat] = useState(52.505);
   const [markerLng, setMarkerLng] = useState(-0.09);
   let position = [lat, lng];
+  const [onClickData, setOnClickData] = useState({});
 
   const myIcon = L.icon({
     iconUrl: require("../../assets/taxi.png"),
@@ -23,10 +27,20 @@ function MapComponent() {
 
   const handleOnClick = (event) => {
     const { lat, lng } = event.latlng;
-    console.log(lat);
-    console.log(lng);
     setMarkerLat(lat);
     setMarkerLng(lng);
+  };
+
+  const onClickPopUp = async (event) => {
+    axios({
+      method: "GET",
+      url: `https://geocode.xyz/${markersLat},${markerLng}?geoit=json`,
+    }).then((value) => {
+      console.log("DATA", value);
+      if (value.data) {
+        setOnClickData(value.data);
+      }
+    });
   };
 
   return (
@@ -43,13 +57,79 @@ function MapComponent() {
       <Marker
         key={`marker-${markerLng}`}
         position={[markersLat, markerLng]}
+        onClick={onClickPopUp}
         icon={myIcon}
       >
-        <Popup>
-          <span>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </span>
-        </Popup>
+        {onClickData && (
+          <Popup>
+            <div>
+              <div className="row container">
+                {typeof onClickData.timezone === "string" ? (
+                  <p className="text-success">
+                    Timezone: {onClickData.timezone}
+                  </p>
+                ) : (
+                  <div>
+                    <Loader type="Oval" height={20} width={20} timeout={2000} />
+                    {!onClickData.timezone && (
+                      <p className="text-danger">
+                        Timezone is not available here!
+                      </p>
+                    )}
+                  </div>
+                )}
+              </div>
+              <div className="row container">
+                {typeof onClickData.state === "string" ? (
+                  <p className="text-success"> State: {onClickData.state}</p>
+                ) : (
+                  <div>
+                    <Loader type="Oval" height={20} width={20} timeout={2000} />
+                    {!onClickData.state && (
+                      <p className="text-danger">
+                        State is not available here!
+                      </p>
+                    )}
+                  </div>
+                )}
+              </div>
+              <div className="row container">
+                {typeof onClickData.staddress === "string" ? (
+                  <p className="text-success">
+                    St Address: {onClickData.staddress}
+                  </p>
+                ) : (
+                  <div>
+                    <Loader type="Oval" height={20} width={20} timeout={2000} />
+                    {!onClickData.staddress && (
+                      <p className="text-danger">
+                        Address is not available here!
+                      </p>
+                    )}
+                  </div>
+                )}
+              </div>
+              <div className="row container">
+                {typeof onClickData.region === "string" ? (
+                  <p className="text-success">Region: {onClickData.region}</p>
+                ) : (
+                  <div>
+                    <Loader type="Oval" height={20} width={20} timeout={2000} />
+                    {!onClickData.region && (
+                      <p className="text-danger">
+                        Region is not available here!
+                      </p>
+                    )}
+                  </div>
+                )}
+              </div>
+            </div>
+            <Button className="btn primary mr-3">
+              Generate nearest vehicle
+            </Button>
+            <Button className="btn cancel">Close</Button>
+          </Popup>
+        )}
       </Marker>
     </Map>
   );
