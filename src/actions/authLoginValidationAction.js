fd42f6a32174cea8ac43d805f8096fc16dcc2783
Rita Selimi <rs40368@ubt-uni.net>
2020-07-13 18:56:48 +0200

    'Validation'

--- src/actions/authLoginValidationAction.js
+++ src/actions/authLoginValidationAction.js
@@ -4,17 +4,25 @@ import {
 } from "../constants";
 
 const handleLoginValidation = (username) => {
-    if(username.length < 3 && username.length > 0){
-        return {
-            type : LOGIN_VALIDATION_ERROR,
-            payload : 'Invalid username. Minimum 3 characters required',
+    if (username) {
+        if (username.length < 3 && username.length > 0) {
+            return {
+                type: LOGIN_VALIDATION_ERROR,
+                payload: 'Invalid username. Minimum 3 characters required',
+            }
+        } else {
+            return {
+                type: LOGIN_INPUT_VALIDATED,
+                payload: false,
+            }
         }
-    } else{
-        return{
-            type : LOGIN_INPUT_VALIDATED,
-            payload : false,
+    }else {
+        return {
+            type: LOGIN_VALIDATION_ERROR,
+            payload: 'Invalid username. Minimum 3 characters required',
         }
     }
 }
+
 
 export default handleLoginValidation;
--- src/actions/authRegisterValidationActions.js
+++ src/actions/authRegisterValidationActions.js
@@ -7,25 +7,32 @@ const passRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9])
 const emailRegex= /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
 
 const handleRegisterValidation = (username, password, email) => {
-    if(username.length < 3 && username.length > 0){
-        return {
-            type : REGISTER_VALIDATION_ERROR,
-            payload : 'Invalid username. Minimum 3 characters required',
+    if (username && password && email) {
+        if (username.length < 3 && username.length > 0) {
+            return {
+                type: REGISTER_VALIDATION_ERROR,
+                payload: 'Invalid username. Minimum 3 characters required',
+            }
+        } else if (!passRegex.test(password)) {
+            return {
+                type: REGISTER_VALIDATION_ERROR,
+                payload: 'Invalid password. Minimum 6 letters and numbers are required',
+            }
+        } else if (!emailRegex.test(email)) {
+            return {
+                type: REGISTER_VALIDATION_ERROR,
+                payload: 'Invalid email address',
+            }
+        } else {
+            return {
+                type: REGISTER_INPUT_VALIDATED,
+                payload: false,
+            }
         }
-    } else if(!passRegex.test(password)){
+    } else {
         return {
             type: REGISTER_VALIDATION_ERROR,
-            payload: 'Invalid password. Minimum 6 letters and numbers are required',
-        }
-    } else if(!emailRegex.test(email)) {
-        return {
-            type: REGISTER_VALIDATION_ERROR,
-            payload: 'Invalid email address',
-        }
-    } else{
-        return{
-            type : REGISTER_INPUT_VALIDATED,
-            payload : false,
+            payload: 'Enter Data',
         }
     }
 }
